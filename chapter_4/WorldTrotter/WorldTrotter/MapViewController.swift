1399edf407864f091ed53f8197efe7752d008fc6
Carlos Bautista <cbi210@nyu.edu>
2016-03-01 18:15:36 -0500

    chapter 6

--- chapter_4/WorldTrotter/WorldTrotter/MapViewController.swift
+++ chapter_4/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,9 +7,36 @@
 //
 
 import UIKit
+import MapKit
+
 class MapViewController: UIViewController {
-    override func viewDidLoad() {
-        //Always call the super implementation of viewDidLoad super.viewDidLoad()
-        print ("MapViewController loaded its view.")
+    
+    var mapView: MKMapView!
+    override func loadView() {
+        //Create a map view
+        mapView = MKMapView()
+        
+        //Set it as *the* view of this controller
+        view = mapView
+        
+        segmentedControl.backgroundColor = UIColor.whiteColor() .colorWithAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview (segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor)
+        
+        topConstraint.active = true
+        leadingConstraint.active = true
+        trailingConstraint.active = true
+        
     }
+    let segmentedControl = UISegmentedControl (items: ["Standard","Hybrid","Satellite"])
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+            }
 }
