31bbdb83fb7e9269f4f01669304742c75edbb772
Carlos Bautista <cbi210@nyu.edu>
2016-03-09 16:26:55 -0500

    animating constraints

--- chapter_1/Quiz/ViewController.swift
+++ chapter_1/Quiz/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var answerLabel: UILabel!
     
     
@@ -45,7 +47,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentQuestionLabel.text = questions[currentQuestionIndex]
+        let question = questions[currentQuestionIndex]
+        currentQuestionLabel.text = question
+        updateOffScreenLabel()
+    }
+    func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -54,16 +62,24 @@ class ViewController: UIViewController {
     }
     func animateLabelTransitions () {
         //Animate the alpha
+        //and the center X constraints
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
         UIView.animateWithDuration(0.5,
             delay: 0,
             options:[],
             animations: {
                 self.currentQuestionLabel.alpha = 0
                 self.nextQuestionLabel.alpha = 1
+                self.view.layoutIfNeeded()
         },
             completion: { _ in
                 swap(&self.currentQuestionLabel,
                     &self.nextQuestionLabel)
+                swap(&self.currentQuestionLabelCenterXConstraint,
+                    &self.nextQuestionLabelCenterXConstraint)
+                self.updateOffScreenLabel()
         })
         
     }
