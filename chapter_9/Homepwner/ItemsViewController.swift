c11a498464e93f446b03403512d26ec777f5fde5
Carlos Bautista <cbi210@nyu.edu>
2016-03-16 18:57:20 -0400

    chapter 9 completed without challenges

--- chapter_9/Homepwner/ItemsViewController.swift
+++ chapter_9/Homepwner/ItemsViewController.swift
@@ -17,9 +17,9 @@ class ItemsViewController: UITableViewController {
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        //Create an instance of UITableViewCell, with defalut appearance
+        //Get a new or recicle cell
         
-        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         //Set the text on the cell with the description of the item
         //that is at the nth index of items, where n = row this cell
         //will appear in on the table view
@@ -30,6 +30,16 @@ class ItemsViewController: UITableViewController {
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         
         return cell
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Get the heigth of the status bar
+        
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
     }
     
 }
